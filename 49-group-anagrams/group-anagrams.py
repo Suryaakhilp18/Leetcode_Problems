54f50e6a5df3deb087df25e66aab75bad0f74f0c
Suryaakhilp18 <suryaakhil232253@gmail.com>
2026-09-10 10:02:10 +0530

    Time: 13 ms (49.20%) | Memory: 22.1 MB (53.32%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,10 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
 
-        ans = defaultdict(list) #--->default values isthaadi
+        ans = defaultdict(list)
 
         for i in strs:
             keys = "".join(sorted(i))
             ans[keys].append(i)
-        
+
         return list(ans.values())
